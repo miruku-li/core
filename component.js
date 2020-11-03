7afd355f37b828631b745ef7bfe8f15e1a6470b2
tscherter <vincent@tscherter.net>
2020-11-03 10:36:11 +0100

    Create component.js

---
+++ component.js
@@ -0,0 +1,17 @@
+import HyperHTMLElement from "/vendor/hyperhtmlelement.js"
+import merge from "/vendor/mergerino.js"
+
+export default class extends HyperHTMLElement {
+	
+	constructor(props) {
+		super().attachShadow({mode: 'open'})
+		Object.assign(this, props)  
+	}
+	
+	patch(payload, render, notify) {
+		const newState = merge(this.state, payload)
+		this.state = newState;
+		if (render!=false) this.render();
+		if (render!=false) this.dispatchEvent(new CustomEvent('change', {detail: newState}))
+	}
+}
