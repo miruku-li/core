1220154c2e086d8bdc128501da6b5d08cda20c01
Vincent Tscherter <vincent@tscherter.net>
2020-11-10 20:37:20 +0100

    stage befor adding merge to fragment

--- urlpage.js
+++ urlpage.js
@@ -2,41 +2,29 @@ import m from '/vndr/mithril.mod.js'
 import b from '/vendor/bss.js'
 import {encode, decode} from '/vendor/lzstring.js'
 import {debounce} from './utils.js'
+import FS from './fragment.js'
 
-const {log} = console
-let value, data, sync
+const {log, clear} = console; clear()
+
+let value;
 
 const index = {
   oninit: () => {
-    value = data = read()
-    sync = true
-    window.onhashchange = () => {
-      data = read()
-      if (value!=data) {
-          value = data
-      } sync = true
+    value = FS.get('value')
+    FS.on = () => {
+      value = FS.get('value')
       m.redraw()
     }
   }, view: () => m('div'+b`d flex; fd column`,
     m('textarea'+b`h 10em`, {
+      value,
       oninput: ({target}) => {
         value = target.value
-        sync = false
-        dWrite(value)
+        FS.set('value', value)
       }
-    }, value ?? data),
-    m('pre', `hash[${sync?'in':'out of'} sync]: ${window.location.hash}`),
+    })
   )
 }
 
 
 m.mount(document.body, index)
-
-function read() {
-  return decode(window.location.hash.slice(1))
-}
-
-function write(data) {
-  window.location.hash = encode(data)
-}
-const dWrite = debounce(write, 666)
