793f045b41d8e0f0705c427432cc97b32b7c616e
Vincent Tscherter <vincent@tscherter.net>
2020-11-10 09:56:21 +0100

    utils: dispatch, debounce, queryParam

--- utils.js
+++ utils.js
@@ -5,4 +5,13 @@ const debounce = (callback, wait = 666, timeout) => (...args) => (
     timeout = setTimeout(() => callback(...args), wait)
 )
 
-export {debounce}
+const dispatch = (target, type, message) =>
+  target.dispatchEvent( new CustomEvent(type, { detail : { message }}));
+
+const queryParam = (url, param, defaultValue) => {
+  url = new URL(url)
+  let searchParams = new URLSearchParams(url.search)
+  return searchParams.get(param) ?? defaultValue
+}
+
+export {debounce, dispatch, queryParam}
